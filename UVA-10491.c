080c37f41a0b5a0f170c7f36a7c078df8ddc0623
Juntian Tao <taozhijei@163.com>
2018-02-12 06:40:30 +0800

    add UVA-10491.c

---
+++ UVA-10491.c
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<climits>
+#include<cmath>
+#include<ctime>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<list>
+#include<set>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+#define FOR(i,a,b) for(register int i=(a);i<(b);++i)
+#define FORR(i,a,b) for(register int i=(a);i<=(b);++i)
+#define ROR(i,a,b) for(register int i=(a);i>=(b);--i)
+#define RORR(i,a,b) for(register int i=(a);i>(b);--i)
+#define PQ priority_queue
+#define VR vector
+#define MST(a,b) memset(a,b,sizeof(a))
+#define SIZ(a) sizeof(a)
+#define CE() clear()
+#define FGETS(s) fgets(s,sizeof(s),stdin)
+#define RE register
+#define FIN(s) freopen(s,"r",stdin)
+#define FOUT(s) freopen(s,"w",stdout)
+typedef long long LL;
+typedef long long unsigned LLU;
+
+int main(void){
+  double a,b,c;
+  while(~scanf("%lf%lf%lf",&a,&b,&c)){
+    double posi = b/(a+b)*(b-1)/(a+b-c-1) + a/(a+b)*(b)/(a+b-c-1);
+    printf("%.5lf\n",posi);
+  }
+
+  return 0;
+}
